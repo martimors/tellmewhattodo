06372648bd8579cd665312336390627388b23f5e
Martin Morset <mmo@one.com>
2022-08-08 11:09:39 +0200

    fix: Change sorting

--- tellmewhattodo/app/app.py
+++ tellmewhattodo/app/app.py
@@ -9,7 +9,7 @@ storage_client = client()
 
 df = storage_client.read()
 df["active"] = df["active"].map({"True": True, "False": False})
-df = df.sort_values(by="datetime")
+df = df.sort_values(by=["active", "datetime"], ascending=(0, 1))
 
 
 def highlight_active(s):
@@ -19,9 +19,9 @@ def highlight_active(s):
         return ["background-color: green"] * len(s)
 
 
-
 # Interactive
 col1, col2 = st.columns([10, 1])
+df = df.sort_values(by=["active", "datetime"], ascending=(0, 1))
 description = df["id"] + ": " + df["description"]
 with col1:
     option = st.selectbox("Alert", description)
