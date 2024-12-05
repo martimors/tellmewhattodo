b0e4c0576f0246d1a9cae751b1555154b17320cb
Martin Morset <mmo@one.com>
2024-12-05 21:12:48 +0100

    helm push

---
+++ build-and-push-chart.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e -o pipefail
+
+helm package ./charts/tmwtd
+CHART=tmwtd-$(helm show chart ./charts/tmwtd | grep '^version:' | awk '{print $2}').tgz
+
+echo $CHART
+
+helm push $CHART oci://registry-1.docker.io/dingobar
