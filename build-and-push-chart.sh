e861ab08a0a12ce92c3ff31275e5367e4ed21eb8
Martin Morset <mmo@one.com>
2024-12-05 21:17:49 +0100

    wip

--- build-and-push-chart.sh
+++ build-and-push-chart.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e -o pipefail
 
+helm dependency build ./charts/tmwtd
 helm package ./charts/tmwtd
 CHART=tmwtd-$(helm show chart ./charts/tmwtd | grep '^version:' | awk '{print $2}').tgz
 
